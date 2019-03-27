0adda1f10f8ac399b4e5ec9eb2ba1d901a433a6c
Nicolas Herbaut <nicolas.herbaut@univ-paris1.fr>
2019-03-27 10:25:11 +0100

    moved to new repo

---
+++ src/main/java/fr/pantheonsorbonne/cri/SpringBootWebApplication.java
@@ -0,0 +1,35 @@
+package fr.pantheonsorbonne.cri;
+
+import java.util.Deque;
+import java.util.List;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import fr.pantheonsorbonne.cri.requirements.Requirement;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+
+@SpringBootApplication
+public class SpringBootWebApplication extends SpringBootServletInitializer {
+
+	public static Deque<Requirement> req=new ConcurrentLinkedDeque<>();
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootWebApplication.class);
+	}
+
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(SpringBootWebApplication.class, args);
+
+		Server server = ServerBuilder.forPort(8081).addService(new CollectorImpl() ).build();
+
+		server.start();
+		server.awaitTermination();
+	}
+
+}
---
+++ src/main/java/fr/pantheonsorbonne/cri/WelcomeController.java
@@ -0,0 +1,19 @@
+package fr.pantheonsorbonne.cri;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+
+	@RequestMapping("/")
+	public String welcome(Map<String, Object> model) {
+		model.put("message", SpringBootWebApplication.req);
+		return "welcome";
+	}
+
+}
