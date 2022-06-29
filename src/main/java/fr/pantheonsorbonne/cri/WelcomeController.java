5d103847af4072daaa068f34ad7d64a9b489cbd7
Nicolas Herbaut <nicolas.herbaut@gmail.com>
2022-06-30 00:42:13 +0200

    changed artifact and group id

--- src/main/java/fr/pantheonsorbonne/cri/WelcomeController.java
+++ src/main/java/fr/pantheonsorbonne/cri/WelcomeController.java
@@ -1,118 +1,109 @@
 package fr.pantheonsorbonne.cri;
 
-import java.io.BufferedReader;
+import org.kohsuke.github.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.HtmlUtils;
+
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-import org.kohsuke.github.GHIssue;
-import org.kohsuke.github.GHIssueState;
-import org.kohsuke.github.GHLabel;
-import org.kohsuke.github.GHRepository;
-import org.kohsuke.github.GitHub;
-import org.kohsuke.github.GitHubBuilder;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.HtmlUtils;
-
 @Controller
 public class WelcomeController {
 
-	@RequestMapping("/")
-	public String welcome(Map<String, Object> model, @RequestParam(value = "githubRepoName", required = false) String githubRepoName) {
-		model.put("message", SpringBootWebApplication.req);
-		List<GHIssue> issues = new ArrayList<>();
-		try {
-			BufferedReader br = new BufferedReader(new FileReader("/github_oauth.txt"));
-			GitHub github = new GitHubBuilder().withOAuthToken(br.readLine()).build();
-			if(githubRepoName == null || githubRepoName.isEmpty()) {
-				githubRepoName = "nh-group/basic-cli-uni/";
-			}
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String main() {
+        return "root";
+    }
 
-			GHRepository repo = github.getRepository(githubRepoName );
+    @RequestMapping(value = "/main", method = {RequestMethod.POST, RequestMethod.GET})
+    public String welcome(Map<String, Object> model, @RequestParam(value = "githubRepoName", required = false) String githubRepoName,
+                          @RequestParam(value = "githubToken", required = true) String githubToken) {
+        model.put("message", SpringBootWebApplication.req);
+        List<GHIssue> issues = new ArrayList<>();
+        try {
 
-			for (GHIssue issue : repo.getIssues(GHIssueState.ALL)) {
-				if(repo.getFullName().equals("nh-group/basic-cli-uni") ) {
-					if (issue.getLabels().stream().map((GHLabel l) -> l.getName()).collect(Collectors.toList())
-							.contains("user stories")) {
-						issues.add(issue);
-					}
-				} else {
-					issues.add(issue);
-				}
+            GitHub github = new GitHubBuilder().withOAuthToken(githubToken).build();
+            if (githubRepoName == null || githubRepoName.isEmpty()) {
+                githubRepoName = "nh-group/basic-cli-uni/";
+            }
 
-			}
-			
-			issues.sort( (GHIssue i1,GHIssue i2) -> Integer.compare(i1.getNumber(),i2.getNumber() ));
+            GHRepository repo = github.getRepository(githubRepoName);
 
-			model.put("issues", issues);
-			model.put("githubRepoName", repo.getFullName());
+            for (GHIssue issue : repo.getIssues(GHIssueState.ALL)) {
+                if (repo.getFullName().equals("nh-group/basic-cli-uni")) {
+                    if (issue.getLabels().stream().map((GHLabel l) -> l.getName()).collect(Collectors.toList())
+                            .contains("user stories")) {
+                        issues.add(issue);
+                    }
+                } else {
+                    issues.add(issue);
+                }
 
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.exit(-1);
-		}
+            }
 
-		return "welcome";
-	}
+            issues.sort((GHIssue i1, GHIssue i2) -> Integer.compare(i1.getNumber(), i2.getNumber()));
 
-	@RequestMapping("/parameters")
-	public String parameters(Map<String, Object> model, @RequestParam(value = "githubRepoName", required = false) String githubRepoName) {
-		if(githubRepoName == null || githubRepoName.isEmpty()) {
-			githubRepoName = "nh-group/basic-cli-uni/";
-		}
+            model.put("issues", issues);
+            model.put("githubRepoName", repo.getFullName());
 
-		//notify dextorm backend that the repo name has changed
-		String reqBody = "{\"name\": \""+githubRepoName+"\"}";
-		final String url = "http://localhost:3000/project/settings";
-		RestTemplate restTemplate = new RestTemplate();
-		HttpHeaders headers = new HttpHeaders();
-		headers.setContentType(MediaType.APPLICATION_JSON);
-		HttpEntity<String> entity = new HttpEntity<String>(reqBody, headers);
-		restTemplate.postForLocation(url, entity);
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
 
-		model.put("githubRepoName", githubRepoName);
-		return "parameters";
-	}
+        return "welcome";
+    }
 
-	@RequestMapping("/ws")
-	public String we(Map<String, Object> model) {
+    @RequestMapping("/parameters")
+    public String parameters(Map<String, Object> model, @RequestParam(value = "githubRepoName", required = false) String githubRepoName) {
+        if (githubRepoName == null || githubRepoName.isEmpty()) {
+            githubRepoName = "nh-group/basic-cli-uni/";
+        }
 
-		return "ws";
-	}
+        //notify dextorm backend that the repo name has changed
+        String reqBody = "{\"name\": \"" + githubRepoName + "\"}";
+        final String url = "http://localhost:3000/project/settings";
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<String> entity = new HttpEntity<String>(reqBody, headers);
+        restTemplate.postForLocation(url, entity);
 
-	@MessageMapping("/hello")
-	@SendTo("/topic/greetings")
-	public Greeting greeting(HelloMessage message) throws Exception {
-		Thread.sleep(1000); // simulated delay
-		return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
-	}
+        model.put("githubRepoName", githubRepoName);
+        return "parameters";
+    }
 
-	@RequestMapping(value = "/save-metrics", method = RequestMethod.POST)
-	@ResponseBody
-	public void postConfig(@RequestBody String config) throws IOException {
-    	File file = new File(System.getProperty("user.dir")+"\\src\\main\\resources\\static\\metricsConfig.json");
-		FileWriter fileWritter = new FileWriter(file);
-		fileWritter.write(config);
-		fileWritter.close();
-	}
+    @RequestMapping("/ws")
+    public String we(Map<String, Object> model) {
+
+        return "ws";
+    }
+
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(HelloMessage message) throws Exception {
+        Thread.sleep(1000); // simulated delay
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+
+    @RequestMapping(value = "/save-metrics", method = RequestMethod.POST)
+    @ResponseBody
+    public void postConfig(@RequestBody String config) throws IOException {
+        File file = new File(System.getProperty("user.dir") + "\\src\\main\\resources\\static\\metricsConfig.json");
+        FileWriter fileWritter = new FileWriter(file);
+        fileWritter.write(config);
+        fileWritter.close();
+    }
 }
